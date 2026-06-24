7da2d7ea079993f9b6578946a499b7d2fda801ee
Vladimir Kidiaev <krios2146@gmail.com>
2026-06-24 18:22:43 +0400

    feat(main): update main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/zhukovsd/roadmap-projects-api-test-runner/internal/database"
+	"github.com/zhukovsd/roadmap-projects-api-test-runner/internal/logger"
+	"github.com/zhukovsd/roadmap-projects-api-test-runner/internal/middleware"
+	testrun "github.com/zhukovsd/roadmap-projects-api-test-runner/internal/test_run"
+)
+
+const Addr = ":8080"
 
 func main() {
-	fmt.Println("xdd")
+	connPool, err := database.ConnectionPool()
+	if err != nil {
+		panic(logger.Fatal("Failed to acquire connection pool for database", "error", err))
+	}
+	if err := database.RunMigrations(); err != nil {
+		panic(logger.Fatal("Failed to run migrations", "error", err))
+	}
+
+	handler := testrun.NewHandler(testrun.NewService(testrun.NewStore(connPool)))
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /api/tests", handler.Create)
+	mux.HandleFunc("GET /api/tests/{id}", handler.FindByID)
+	mux.HandleFunc("GET /api/tests", handler.List)
+
+	logger.Info("Starting HTTP server", "address", Addr)
+
+	if err := http.ListenAndServe(Addr, middleware.Auth(mux)); err != nil {
+		logger.Error("Failed to start HTTP server", "error", err)
+		os.Exit(1)
+	}
 }
