6180ee26532b73cbdbbc7ad7b5a2d53f10275a34
Vladimir Kidiaev <krios2146@gmail.com>
2026-06-25 19:55:59 +0400

    feat(service): add test suite execution after test run is created

--- internal/test_run/service.go
+++ internal/test_run/service.go
@@ -2,7 +2,9 @@ package testrun
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/zhukovsd/roadmap-projects-api-test-runner/internal/logger"
@@ -19,10 +21,18 @@ func NewService(store *Store) *Service {
 func (s *Service) Create(ctx context.Context, input CreateInput) (TestRun, error) {
 	logger.Info("Creating test run", "input", input)
 
+	suite, err := newTestSuite(input.ProjectName, input.DeployBaseURL)
+	if err != nil {
+		return TestRun{}, fmt.Errorf("service.Create: %w", err)
+	}
+
 	testRun, err := s.store.Create(ctx, input)
 	if err != nil {
 		return TestRun{}, fmt.Errorf("service.Create: %w", err)
 	}
+
+	go s.runTestSuite(suite, testRun)
+
 	return testRun, nil
 }
 
@@ -44,4 +54,42 @@ func (s *Service) List(ctx context.Context, filters Filters) ([]TestRun, error) 
 		return nil, fmt.Errorf("service.List: %w", err)
 	}
 	return testRuns, nil
+}
+
+func (s *Service) runTestSuite(suite Suite, testRun TestRun) {
+	logger.Info("Running test suite", "project", testRun.ProjectName, "telegramUserID", testRun.TelegramUserID)
+
+	testResults := suite.Run()
+	completedAt := time.Now().Unix()
+
+	reportBytes, err := json.Marshal(testResults)
+	if err != nil {
+		logger.Error("Failed to marshal report: %w", err)
+
+		error := "Failed to marshal report"
+
+		err = s.store.Update(context.Background(), testRun.ID, UpdateInput{
+			CompletedAt: &completedAt,
+			Status:      StatusCompleted,
+			Error:       &error,
+		})
+		if err != nil {
+			logger.Error("Failed to update test run", "error", err, "testRun", testRun)
+		}
+		return
+	}
+
+	report := json.RawMessage(reportBytes)
+
+	err = s.store.Update(context.Background(), testRun.ID, UpdateInput{
+		CompletedAt: &completedAt,
+		Status:      StatusCompleted,
+		Error:       nil,
+		Report:      &report,
+	})
+	if err != nil {
+		logger.Error("Failed to update test run", "error", err, "testRun", testRun)
+	}
+
+	logger.Info("Test suite completed", "project", testRun.ProjectName, "telegramUserID", testRun.TelegramUserID)
 }
