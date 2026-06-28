b8c71ef1257d8fcf86b4886f1a35e8bdf21f9a0f
Vladimir Kidiaev <krios2146@gmail.com>
2026-06-28 21:17:06 +0400

    feat(suite_store): add in-memory suite store

---
+++ internal/test_run/suite_store.go
@@ -0,0 +1,27 @@
+package testrun
+
+import "sync"
+
+type SuiteStore struct {
+	m sync.Map
+}
+
+func NewSuiteStore() *SuiteStore {
+	return &SuiteStore{}
+}
+
+func (s *SuiteStore) Get(id string) (Suite, bool) {
+	val, found := s.m.Load(id)
+	if !found {
+		return nil, false
+	}
+	return val.(Suite), true
+}
+
+func (s *SuiteStore) Set(id string, suite Suite) {
+	s.m.Store(id, suite)
+}
+
+func (s *SuiteStore) Delete(id string) {
+	s.m.Delete(id)
+}
