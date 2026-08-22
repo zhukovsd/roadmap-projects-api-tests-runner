225cf99edd7fa3840e38893b74afebbdca19fea8
Vladimir Kidiaev <krios2146@gmail.com>
2026-08-22 19:33:57 +0400

    feat(tests): add constants for parsing req/resp in JSON stream

--- tests/common.go
+++ tests/common.go
@@ -2,3 +2,7 @@
 package tests
 
 const TestDescriptionAttr = "description"
+const TestRequestStartMarker = "REQUEST_START"
+const TestRequestEndMarker = "REQUEST_END"
+const TestResponseStartMarker = "RESPONSE_START"
+const TestResponseEndMarker = "RESPONSE_END"
