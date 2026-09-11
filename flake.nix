7f6b535a7ef3ac59ddcdb459e461fbe922b063c4
Vladimir Kidiaev <krios2146@gmail.com>
2026-09-11 17:16:40 +0400

    feat(dev): add flake.nix with dev shell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          gopls
+          gotools
+          just
+        ];
+      };
+    };
+}
